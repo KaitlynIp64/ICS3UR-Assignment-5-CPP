333e5e2807b73c527e193b467c89fd773dfd2fd8
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-11-28 03:59:53 +0000

    fixed code

--- wordRepeat.cpp
+++ wordRepeat.cpp
@@ -8,31 +8,31 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+using-declarations std;
 
 int main() {
     // this function defines the variables
     std::string integerAsString;
+    int numInput;
     std::string repeat;
     std::string wordInput;
 
     // this function is the process to multiply a word n amount of times
 
-
     std::cout << "Enter a word: ";
     std::cin >> wordInput;
     std::cout << "Enter the amount of times you want the word to repeat: ";
-    
-        std::cin >> numInput;
-    }
-     catch (std::invalid_argument) {
-        std::cout << "That was not a valid input.";
-    }
+    std::cin >> integerAsString;
+
+    try {
+        int numInput = std::stoi(integerAsString);
 
         for (int loopCounter = 1; loopCounter <= numInput; loopCounter++)
             repeat += wordInput;
         cout << repeat;
-        return 0;
-    
-        std::cout << "\nDone." << std::endl;
+    } catch (std::invalid_argument) {
+        std::cout << "That was not a valid input.";
     }
+    std::cout << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
